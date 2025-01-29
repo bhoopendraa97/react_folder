a252a45f9913331a85eb6eca28b7c02c03cc59c1
bhoopendraa97 <bhoopendraa77@gmail.com>
2025-01-30 00:23:07 +0530

    props

---
+++ props/src/App.jsx
@@ -0,0 +1,93 @@
+
+
+
+
+
+/*    const App=()=>{
+        return(
+         <>
+         <h1 className="name">Application form</h1>
+         Name: <input type="text"/>
+         <br /><br />
+         City: <input type="text"/>
+         <hr size="4" color="red" />
+          <button>Save</button>
+         
+         </>
+        )
+  }   
+  export default App;  */
+
+  
+
+
+/* const name="mohan";
+const age=3;
+
+const App=()=>{
+    return(
+      <h1>Welcome to Bhopal {name} we are web age{age*3} Developer</h1>
+    )
+}
+export default App;  */
+
+
+/* const sub=<ol>     extra note create nahi hotaa
+   <li>PHP</li>
+   <li>Java</li>
+   <li>Python</li>
+   <li>ASP</li>
+   <li>pre1</li>
+</ol>
+
+const App=()=>{
+    return(
+      <>
+      <h1>our computer subject are : {sub}</h1>
+      </>
+    )
+}   
+export default App; */
+
+
+
+/* import Cybrom from "./Cybrom"
+const App=()=>{
+  return(
+<>
+<h1>Welcome This is App Data</h1>
+     <Cybrom/>
+</>
+  )
+}
+export default App;  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ props/src/Cybrom.jsx
@@ -0,0 +1,60 @@
+/*const Cybrom=()=>{
+  return (
+    <>
+    <h1>Hello Welcome to Cybrom Bhopal!!</h1>
+    </>
+  )
+}
+export default Cybrom; */
+
+
+//import { Children } from "react";
+
+
+
+/*const  Cybrom=({name,city, Children})=>{ 
+  
+  return(
+  <>
+  {Children}
+  <h1>Welcome This Is Cybrom Component</h1>
+  <h1>Name: {name}</h1>
+  <h1>City: {city}</h1>
+  {Children}
+
+  </>
+
+)
+}
+export default Cybrom; */
+
+/* const  Cybrom=({Children})=>{ 
+  return(
+  <>
+  <h1>This is cybrom Component Data</h1>
+  <h1 style={{color:"red"}}>{Children}</h1>
+  </>
+)
+}
+export default Cybrom;  */  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
