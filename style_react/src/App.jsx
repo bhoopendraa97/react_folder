c8cef9ddb2b0c3068ee63b56a5b65c4e63a57c43
bhoopendraa97 <bhoopendraa77@gmail.com>
2025-02-13 13:19:37 +0530

    style

---
+++ style_react/src/App.jsx
@@ -0,0 +1,64 @@
+//Q)5--------------------------------------------------------------------------------------->
+ /*const App=()=>{
+return(
+<>
+<h1 style={{color:"red", fontFamily:"initial",textDecoration:"underline"}}>welcome t bhopal from cybrom!!</h1>
+<h1 style={{color:"green", fontFamily:"serif",textDecoration:"overline"}}>welcome t bhopal from cybrom!!</h1>
+<h1 style={{color:"pink",}}>welcome t bhopal from cybrom!!</h1>
+</>
+     )
+}
+export default App; */
+
+//Q)4------------------------------------------------------------------------------------------------>
+/*const App=()=>{
+return(
+<>
+<div style={{width:"700px",height:"400px",backgroundColor:"pink",margin:'auto',marginTop:"50px",borderRadius:"30px",border:"2px solid black"}}>
+<div style={{width:"500px",height:"250px",backgroundColor:"green",margin:"auto",marginTop:"70px",border:"2px solid black",borderRadius:"30px"}}>
+<div style={{width:"400px",height:"150px",backgroundColor:"red",borderRadius:"30px",margin:"auto",marginTop:"50px"}}>
+     <h1 style={{textAlign:"center",paddingTop:'50px',fontSize:'50px65e'}}>Cybrom</h1>
+</div>
+</div>
+</div>
+</>
+)
+}
+export default App; */
+
+//Q)3------------------------------------------------------------------------->
+ /* const myval={
+color:"green",
+fontSize:"50px",
+backgroundColor:"pink",
+}
+
+const App=()=>{
+return(
+<>
+<h1 style={myval}> welcome to cybrom !!</h1>
+</>
+)
+}
+export default App; */
+
+
+//Q)4----------------------------------------------------------------->
+/* const App=()=>{
+return(
+<>
+<h1 id="demo">welcome to cybrom!!</h1>
+</>
+)
+}
+export default App; */
+
+
+
+
+
+
+
+
+
+
